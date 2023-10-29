03005259282d08936cd3f2567794ad94a084a77c
RonaldZav <100430955+RonaldZav@users.noreply.github.com>
2023-10-28 22:37:13 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch');
+
+module.exports = class Chat {
+  static async create(prompt) {
+    const apiUrl = `https://api.gglvxd.eu.org/v1/chatgpt?q=${encodeURIComponent(prompt)}`;
+    const timeoutMillis = 30000;
+    const timeOutMessage = "Tiempo de espera excedido.";
+    
+    const timeoutPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(new Error(timeOutMessage));
+      }, timeoutMillis);
+    });
+
+    try {
+      const apiResponse = await Promise.race([fetch(apiUrl), timeoutPromise]);
+      if (apiResponse === timeoutPromise) {
+        throw new Error(timeOutMessage);
+      }
+      
+      const response = await apiResponse.json();
+      return response;
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  }
+};
